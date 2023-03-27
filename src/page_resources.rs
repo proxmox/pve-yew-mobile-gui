92f5d8518366ef68b564d0e1cf05509eab9a0c8a
Dietmar Maurer <dietmar@proxmox.com>
2023-03-27 14:29:12 +0200

    implement resouce page

--- src/page_resources.rs
+++ src/page_resources.rs
@@ -1,22 +1,21 @@
 use std::rc::Rc;
 
-use js_sys::Date;
-use wasm_bindgen::JsValue;
+use anyhow::Error;
 
-use yew::prelude::*;
-use yew_router::scope_ext::RouterScopeExt;
 use yew::virtual_dom::{VComp, VNode};
 
 use pwt::prelude::*;
-use pwt::touch::{Fab};
-use pwt::widget::{Button, Column, Container, Dialog, Row};
+use pwt::touch::Fab;
 use pwt::widget::form::{Field, Form, FormContext};
+use pwt::widget::{Button, Card, Column, Container, Dialog, Row};
+
+use proxmox_client::api_types::{ClusterResourceKind, ClusterResources, ClusterResourcesType};
+use proxmox_yew_comp::http_get;
 
 use crate::{Route, TopNavBar};
 
 #[derive(Clone, PartialEq, Properties)]
-pub struct PageResources {
-}
+pub struct PageResources {}
 
 impl PageResources {
     pub fn new() -> Self {
@@ -25,27 +24,162 @@ impl PageResources {
 }
 
 pub struct PvePageResources {
+    data: Result<Vec<ClusterResources>, String>,
 }
 
 pub enum Msg {
+    LoadResult(Result<Vec<ClusterResources>, Error>),
+}
+
+impl PvePageResources {
+    fn load(&self, ctx: &Context<Self>) {
+        let link = ctx.link().clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let result = http_get("/cluster/resources", None).await;
+            link.send_message(Msg::LoadResult(result));
+        });
+    }
+
+    fn create_vm_list_item(&self, icon: &str, item: &ClusterResources) -> Card {
+        let icon = html! {<i class={
+            classes!(
+                "pwt-font-size-title-large",
+                icon.to_string(),
+                (item.status.as_deref() == Some("running")).then(|| "pwt-color-primary"),
+            )
+        }/>};
+
+        Card::new()
+            .class("pwt-d-flex pwt-gap-2")
+            .class("pwt-shape-none pwt-card-flat pwt-interactive")
+            .class("pwt-scheme-neutral")
+            .padding_x(2)
+            .padding_y(1)
+            .border_bottom(true)
+            .class("pwt-align-items-center")
+            .with_child(icon)
+            .with_child(
+                Column::new()
+                    .class("pwt-flex-fill")
+                    .gap(1)
+                    .with_child(html! {
+                        <div class="pwt-font-size-title-medium">{
+                            format!("{} {}", item.vmid.unwrap(), item.name.as_deref().unwrap_or(""))
+
+                        }</div>
+                    })
+                    .with_child(html! {
+                        <div class="pwt-font-size-title-small">{item.node.as_deref().unwrap()}</div>
+                    }),
+            )
+            .with_child(html! {
+                <div class="pwt-font-size-title-small">{item.status.as_deref().unwrap_or("")}</div>
+            })
+    }
+
+    fn create_qemu_list_item(&self, ctx: &Context<Self>, item: &ClusterResources) -> Html {
+        let icon = "fa fa-fw fa-desktop";
+        self.create_vm_list_item(icon, item).into()
+    }
+
+    fn create_lxc_list_item(&self, ctx: &Context<Self>, item: &ClusterResources) -> Html {
+        let icon = "fa fa-fw fa-cube";
+        self.create_vm_list_item(icon, item).into()
+    }
+
+    fn create_storage_list_item(&self, ctx: &Context<Self>, item: &ClusterResources) -> Html {
+        let row1 = Row::new()
+            .gap(2)
+            .class("pwt-align-items-flex-end")
+            .with_child(
+                Column::new()
+                    .class("pwt-flex-fill")
+                    .gap(1)
+                    .with_child(html! {
+                        <div class="pwt-font-size-title-medium">{item.storage.as_deref().unwrap()}</div>
+                    })
+                    .with_child(html! {
+                        <div class="pwt-font-size-title-small">{item.node.as_deref().unwrap()}</div>
+                    }),
+            )
+            .with_child(html!{
+                <div class="pwt-font-size-title-small">{item.status.as_deref().unwrap_or("")}</div>
+            });
+
+        let icon = html! {<i class={
+            classes!(
+                "pwt-font-size-title-large",
+                "fa",
+                "fa-database",
+                (item.status.as_deref() == Some("available")).then(|| "pwt-color-primary"),
+            )
+        }/>};
+
+        let used = format!("{:.2} Gib", (item.disk.unwrap_or(0) as f64) / (1024.0 * 1024.0 *1024.0));
+        let total = format!("{:.2} Gib", (item.maxdisk.unwrap_or(0) as f64) / (1024.0 * 1024.0 *1024.0));
+
+        let row2 = Row::new()
+            .gap(2)
+            .class("pwt-align-items-flex-end")
+            .with_child(icon)
+            .with_child(html!{
+                <div class="pwt-font-size-title-medium pwt-flex-fill">{used}</div>
+            })
+            .with_child(html!{
+                <div class="pwt-font-size-title-medium">{format!("Total: {}", total)}</div>
+            });
+
+        Card::new()
+            .class("pwt-d-flex pwt-flex-column pwt-gap-1")
+            .class("pwt-shape-none pwt-card-flat pwt-interactive")
+            .class("pwt-scheme-neutral")
+            .padding_x(2)
+            .padding_y(1)
+            .border_bottom(true)
+            .with_child(row1)
+            .with_child(row2)
+            .into()
+    }
+
+    fn create_resource_list(&self, ctx: &Context<Self>, data: &[ClusterResources]) -> Html {
+        let children = data.iter().filter_map(|item| match item.ty {
+            ClusterResourcesType::Qemu => Some(self.create_qemu_list_item(ctx, item)),
+            ClusterResourcesType::Lxc => Some(self.create_lxc_list_item(ctx, item)),
+            ClusterResourcesType::Storage => Some(self.create_storage_list_item(ctx, item)),
+            _ => None,
+        });
+        Column::new().children(children).into()
+    }
 }
 
 impl Component for PvePageResources {
     type Message = Msg;
     type Properties = PageResources;
 
-    fn create(_ctx: &Context<Self>) -> Self {
-
-        Self {}
+    fn create(ctx: &Context<Self>) -> Self {
+        let me = Self {
+            data: Err(format!("no data loaded")),
+        };
+        me.load(ctx);
+        me
     }
 
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::LoadResult(result) => {
+                self.data = result.map_err(|err| err.to_string());
+            }
+        }
+        true
+    }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let props = ctx.props();
 
-        let content = Column::new()
-            .padding(2)
-            .with_child("This is the Resources page.");
+        let content = match &self.data {
+            Err(err) => pwt::widget::error_message(err, "pwt-p-2"),
+            Ok(data) => self.create_resource_list(ctx, &data),
+        };
 
         let fab = Container::new()
             .class("pwt-position-absolute")
