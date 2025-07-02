39d2a7744963109fdd7b7de92463997e46ba73cb
Dietmar Maurer <dietmar@proxmox.com>
2025-07-02 11:11:19 +0200

    add debugging code to find dialog problems
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- src/widgets/volume_action_dialog.rs
+++ src/widgets/volume_action_dialog.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
 
 use proxmox_human_byte::HumanByte;
+use proxmox_yew_comp::{default_confirm_remove_message, ConfirmButton};
 use yew::virtual_dom::{VComp, VNode};
 
 use pwt::prelude::*;
@@ -17,6 +18,7 @@ pub fn show_volume_actions<COMP: Component>(
 ) {
     let controller = scope.page_controller().unwrap();
 
+    log::info!("SHOW ACTIONS");
     controller.show_side_dialog(
         SideDialog::new()
             .location(pwt::touch::SideDialogLocation::Bottom)
@@ -44,14 +46,82 @@ impl VolumeActionDialog {
     }
 }
 
+pub enum Msg {
+    Remove,
+    Cancel,
+}
+
 pub struct PveVolumeActionDialog {}
 
+impl PveVolumeActionDialog {
+    fn show_safe_remove_dialog(link: yew::html::Scope<Self>, volid: &str) {
+        let content = Column::new()
+            .class(pwt::css::FlexFit)
+            .class(pwt::css::AlignItems::Center)
+            .padding(2)
+            .gap(2)
+            .with_child(
+                Container::new()
+                    .class("pwt-font-size-title-large")
+                    .with_child(tr!("Attention")),
+            )
+            .with_child(
+                Container::new()
+                    .class("pwt-font-size-title-medium")
+                    .with_child(default_confirm_remove_message(Some(volid))),
+            )
+            .with_child(
+                Row::new()
+                    .with_flex_spacer()
+                    .with_child(
+                        Button::new(tr!("Cancel"))
+                            .class("pwt-button-text")
+                            .on_activate(link.callback(|_| Msg::Cancel)),
+                    )
+                    .with_child(
+                        Button::new(tr!("Confirm"))
+                            .class("pwt-button-text")
+                            .on_activate(link.callback(|_| Msg::Remove)),
+                    ),
+            );
+
+        let controller = link.page_controller().unwrap();
+
+        controller.show_side_dialog(
+            SideDialog::new()
+                .location(pwt::touch::SideDialogLocation::Bottom)
+                .with_child(content),
+        );
+    }
+}
+
 impl Component for PveVolumeActionDialog {
-    type Message = ();
+    type Message = Msg;
     type Properties = VolumeActionDialog;
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {}
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Cancel => {
+                log::info!("CLOSE");
+                let controller = ctx.link().page_controller().unwrap();
+                controller.close_side_dialog();
+            }
+            Msg::Remove => {
+                log::info!("REMOVE");
+                let controller = ctx.link().page_controller().unwrap();
+                controller.close_side_dialog();
+            }
+        }
+        true
+    }
+
+    fn changed(&mut self, ctx: &Context<Self>, _old_props: &Self::Properties) -> bool {
+        log::info!("ACVTION CHANGED");
+        true
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
@@ -88,9 +158,10 @@ impl Component for PveVolumeActionDialog {
                     .disabled(true), // fixme
             )
             .with_child(
-                Button::new(tr!("Remove"))
+                ConfirmButton::remove_entry(props.item.volid.clone())
                     .icon_class("fa fa-trash-o")
-                    .class("pwt-button-outline"),
+                    .class("pwt-button-outline")
+                    .on_activate(ctx.link().callback(|_| Msg::Remove)),
             )
             .with_child(
                 Button::new(tr!("Show Configuration"))
