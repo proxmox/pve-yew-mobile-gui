a02c8d93dd39475e2f63831a0f04efe3419d8d8a
Thomas Lamprecht <t.lamprecht@proxmox.com>
2025-07-31 00:34:01 +0200

    do not re-ship translations but depend on them
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/main.rs
+++ src/main.rs
@@ -296,7 +296,7 @@ impl Component for PveMobileApp {
         MaterialApp::new(render)
             .theme_dir_prefix("/pve2/yew-mobile/css/".into())
             .catalog_url_builder(|lang: &String| {
-                format!("/pve2/yew-mobile/pve-yew-mobile-catalog-{lang}.mo")
+                format!("/yew-mobile/i18n/pve-yew-mobile-catalog-{lang}.mo")
             })
             .into()
     }
