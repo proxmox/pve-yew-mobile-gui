d7b7a204e82d0a30d452757fb9979cc7e091a5f6
Dietmar Maurer <dietmar@proxmox.com>
2025-10-10 16:26:33 +0200

    reassign disk input panel: make it statefule
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- src/pages/page_qemu_status/reassign_disk_dialog.rs
+++ src/pages/page_qemu_status/reassign_disk_dialog.rs
@@ -1,11 +1,70 @@
+use std::rc::Rc;
+
+use yew::virtual_dom::VComp;
+
+use pve_api_types::ClusterResource;
+
 use pwt::prelude::*;
 use pwt::widget::Column;
 
 use crate::form::{PveGuestSelector, PveGuestType, QemuControllerSelector};
-use crate::widgets::EditDialog;
-use crate::widgets::{label_field, PropertyEditorState};
+use crate::widgets::{label_field, EditDialog, PropertyEditorState};
 
-pub fn qemu_reassign_disk_dialog(name: &str, _node: Option<AttrValue>) -> EditDialog {
+#[derive(PartialEq, Properties, Clone)]
+struct QemuReassignDiskPanel {
+    node: Option<AttrValue>,
+    state: PropertyEditorState,
+}
+
+enum Msg {
+    Target(Option<ClusterResource>),
+}
+
+struct QemuReassignDiskPanelComp {
+    target: Option<ClusterResource>,
+}
+
+impl Component for QemuReassignDiskPanelComp {
+    type Message = Msg;
+    type Properties = QemuReassignDiskPanel;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { target: None }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Target(target) => self.target = target,
+        }
+        true
+    }
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        //let props = ctx.props();
+        // let state = &props.state;
+        Column::new()
+            .class(pwt::css::FlexFit)
+            .gap(2)
+            .with_child(label_field(
+                tr!("Target Guest"),
+                PveGuestSelector::new()
+                    .name("target-vmid")
+                    .required(true)
+                    .guest_type(PveGuestType::Qemu)
+                    .on_change(ctx.link().callback(Msg::Target))
+                    .mobile(true),
+                true,
+            ))
+            .with_child(label_field(
+                tr!("Bus/Device"),
+                QemuControllerSelector::new().name("target-disk"),
+                //.exclude_devices(used_devices),
+                true,
+            ))
+            .into()
+    }
+}
+
+pub fn qemu_reassign_disk_dialog(name: &str, node: Option<AttrValue>) -> EditDialog {
     let title = tr!("Reassign Disk");
 
     EditDialog::new(title.clone() + " (" + name + ")")
@@ -20,25 +79,14 @@ pub fn qemu_reassign_disk_dialog(name: &str, _node: Option<AttrValue>) -> EditDi
                 Ok(data)
             }
         })
-        .renderer(|_| {
-            Column::new()
-                .class(pwt::css::FlexFit)
-                .gap(2)
-                .with_child(label_field(
-                    tr!("Target Guest"),
-                    PveGuestSelector::new()
-                        .name("target-vmid")
-                        .required(true)
-                        .guest_type(PveGuestType::Qemu)
-                        .mobile(true),
-                    true,
-                ))
-                .with_child(label_field(
-                    tr!("Bus/Device"),
-                    QemuControllerSelector::new().name("target-disk"),
-                    //.exclude_devices(used_devices),
-                    true,
-                ))
-                .into()
+        .renderer({
+            let node = node.clone();
+            move |state| {
+                let props = QemuReassignDiskPanel {
+                    state,
+                    node: node.clone(),
+                };
+                VComp::new::<QemuReassignDiskPanelComp>(Rc::new(props), None).into()
+            }
         })
 }
